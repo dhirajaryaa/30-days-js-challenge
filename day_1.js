257d749229cdb30bdce1dc86292c54f421d6eb2b
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-07-13 11:04:32 +0530

    Day1: js challenge

---
+++ day_1.js
@@ -0,0 +1,46 @@
+// Activity 1: variable Declaration ----
+// task1:
+var num1 = 45;
+console.log(num1);
+
+//task2:
+let str1 = "Dhiraj Arya";
+console.log(str1);
+
+// Activity 2: Constant Declaration ----
+// task3:
+const data = false;
+console.log(data);
+
+// Activity 3: Data Types ----
+// task4:
+var id = 15;
+console.log(typeof(id));
+var name = "chai aur js challenge" ;
+console.log(typeof(name));
+var bol = true;
+console.log(typeof(bol));
+var obj = {
+    userId: 13,
+    userName: "Dhiraj Arya",
+}
+console.log(typeof(obj));
+var arr = [15,1,56,23,85];
+console.log(typeof(arr));  //know about on this section typeof JavaScript treats arrays as object.
+
+// Activity 4: Data Types ----
+// task5:
+let prev = 24;
+console.log(prev);
+
+prev = 35;
+console.log(prev);
+
+// Activity 5: Data Types ----
+// task6:
+const text = "Dhiraj";
+console.log(text);
+text = "chai";
+console.log(text);  // this point not reassign of value on const.
+
+
