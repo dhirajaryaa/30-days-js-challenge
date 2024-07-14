181bba2c2059ad0c0500576804b9c45883d9cff0
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-07-14 18:24:24 +0530

    Day-2 14-july-2024

---
+++ Day-2/day_2.js
@@ -0,0 +1,33 @@
+// Activity 1: Arithmetic operations ----
+
+var num1 = 45;
+var num2 = 15;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+
+// Activity 2: Assignment operations ----
+var num3 = 12;
+console.log(num3 += num2);
+console.log(num3 -= num2);
+
+// Activity 3: Comparison operations ----
+var num4 = "15";
+console.log(num3 > num2);
+console.log(num3 < num2);
+console.log(num1 <= num2);
+console.log(num1 <= num3);
+console.log(num3 == num2);
+console.log(num4 === num2);
+
+// Activity 4: Logical operations ----
+
+console.log(num1 > num3 && num2 > num3);
+console.log(num1 > num3 || num4 > num1);
+
+// Activity 5: Ternary operations ----
+console.log(num3 ? "Positive":"Negative")
+
+
