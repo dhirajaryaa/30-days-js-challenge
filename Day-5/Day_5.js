e7949f28dc3018cbd4967c740a236fed77fb6f78
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-07-17 19:59:48 +0530

    comp: activity 1 and 2 completed

---
+++ Day-5/Day_5.js
@@ -0,0 +1,31 @@
+// Activity 1: Function Declaration ----
+function checkNumType(num){
+    if(num%2 === 0){
+        console.log("This Number is evan");
+    }else{
+        console.log("This Number is odd");
+    }
+}
+// checkNumType(14);
+
+function square(val){
+    return val*val
+}
+// console.log(square(2));
+
+// Activity 2: Function Expression ----
+const findMaxNum = function(num1,num2){
+    if(num1>num2){
+        console.log(num1,"Greater Than",num2);
+    }else{
+        console.log(num2,"Greater Than",num1);
+    }
+}
+// findMaxNum(15,19);
+
+const Str = function(str1,str2){
+    return str1+str2
+}
+console.log(Str("Dhiraj","Arya"));
+
+// Activity 3: Function Expression ----
