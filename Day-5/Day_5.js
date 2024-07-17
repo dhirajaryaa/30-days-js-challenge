9b6e0c36ac48062e22b36135d567ef380b57a2f2
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-07-17 20:34:53 +0530

    comp: activity 5 "17-july-2024"

--- Day-5/Day_5.js
+++ Day-5/Day_5.js
@@ -1,56 +1,79 @@
 // Activity 1: Function Declaration ----
-function checkNumType(num){
-    if(num%2 === 0){
-        console.log("This Number is evan");
-    }else{
-        console.log("This Number is odd");
-    }
+function checkNumType(num) {
+  if (num % 2 === 0) {
+    console.log("This Number is evan");
+  } else {
+    console.log("This Number is odd");
+  }
 }
 // checkNumType(14);
 
-function square(val){
-    return val*val
+function square(val) {
+  return val * val;
 }
 // console.log(square(2));
 
 // Activity 2: Function Expression ----
-const findMaxNum = function(num1,num2){
-    if(num1>num2){
-        console.log(num1,"Greater Than",num2);
-    }else{
-        console.log(num2,"Greater Than",num1);
-    }
-}
+const findMaxNum = function (num1, num2) {
+  if (num1 > num2) {
+    console.log(num1, "Greater Than", num2);
+  } else {
+    console.log(num2, "Greater Than", num1);
+  }
+};
 // findMaxNum(15,19);
 
-const Str = function(str1,str2){
-    return str1+str2
-}
+const Str = function (str1, str2) {
+  return str1 + str2;
+};
 // console.log(Str("Dhiraj","Arya"));
 
 // Activity 3: Arrow Function ----
-const sumNumber = (num1,num2)=>{
-    return num1+num2
-}
+const sumNumber = (num1, num2) => {
+  return num1 + num2;
+};
 // console.log(sumNumber(15,16));
 
-const checkStr = (str)=>{
-    if(str === "arya"){
-        return true
-    }else{
-        return false
-    }
-}
+const checkStr = (str) => {
+  if (str === "arya") {
+    return true;
+  } else {
+    return false;
+  }
+};
 // console.log(checkStr("arya"));
 // console.log(checkStr("dhirajarya"));
 
 // Activity 4: Function Parameters and Default Values ----
-const data = (param1,param2="soda")=>{
-    console.log(param1+param2);
-}
+const data = (param1, param2 = "soda") => {
+  console.log(param1 + param2);
+};
 // data("backing")
 
-const getMassage = function (name,age=18){
-    return `Good Morning ${name} Your age is ${age}`
+const getMassage = function (name, age = 18) {
+  return `Good Morning ${name} Your age is ${age}`;
+};
+// console.log(getMassage("Gopi"));
+
+// Activity 5 : high order functions
+function higherOrderFunc(sum, num) {
+  sum(num);
 }
-console.log(getMassage("Gopi"));
+function sum(num) {
+  console.log(num+num);
+}
+
+higherOrderFunc(sum,15)
+
+function higherOrderFunc2(callback1,callback2,num){
+    callback1(num)
+    callback2(num)
+}
+function firstFunc(val){
+    console.log("this is callback1",val);
+}
+function secondFunc(val){
+    console.log(`${val} this is callback2`);
+}
+
+higherOrderFunc2(firstFunc,secondFunc,58)
